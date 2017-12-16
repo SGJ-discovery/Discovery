885234e4d4bf1e1e0b98035e6912e3da49e80d2d
yutatakatsu <vocaloid_asax@yahoo.co.jp>
2017-12-17 07:40:54 +0900

    [Aitem]アイテムの生成、取得の実装

---
+++ Discovery/Assets/Scripts/Aitem/GetAitem.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GetAitem : MonoBehaviour {
+
+    private bool hitFlag; // 当たったかどうかのフラグ
+    private GameObject obj; // 当たったオブジェクトを格納する
+
+    void Start()
+    {
+        hitFlag = false;
+    }
+
+    // トリガーとの接触時に呼ばれるコールバック
+    void OnTriggerEnter(Collider hit)
+    {
+        // 接触対象はPlayerタグですか？
+        if (hit.CompareTag("Aitem"))
+        {
+            Debug.Log("hit");
+            hitFlag = true;
+            obj = hit.gameObject;
+        }
+        else
+        {
+            hitFlag = false;
+        }
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown("e"))
+        {
+            // このコンポーネントを持つGameObjectを破棄する
+            Destroy(obj.gameObject);
+        }
+    }
+}
