a043ea25d107a8952a18546aaa8d1b680cae13df
Machida <aya14138@gmail.com>
2017-12-16 16:46:10 +0900

    [FlasingText]チームロゴ、スタートを促すテキストのアニメーション(点滅)の実装

---
+++ Discovery/Assets/Scripts/Title/FlasingText.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FlasingText : MonoBehaviour {
+
+    //テキストの点滅
+
+    Text StartText;
+
+
+    bool Flasing = false;
+
+
+
+	// Use this for initialization
+	void Start () {
+
+        StartText = GetComponent<Text>();
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (Flasing == false)
+        {
+            StartText.enabled = true;
+            Flasing = true;
+            Invoke("FlasingIN", 1.0f);
+        }
+       
+
+    }
+
+
+
+    void FlasingIN()
+    {
+            StartText.enabled = false;
+            Invoke("FlasingOUT", 1.0f);
+    }
+    
+
+    void FlasingOUT()
+    {
+        Flasing = false;
+    }
+
+   
+
+
+
+}
