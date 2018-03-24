6a47a1339ec38687aabc0af92b65c82226920e9b
Machida <aya14138@gmail.com>
2018-03-24 17:42:32 +0900

    【町田】ヘルプ表示

---
+++ Discovery/Assets/Scripts/UI/HelpControl.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HelpControl : MonoBehaviour {
+
+
+    public GameObject HelpPage;
+
+    private bool OC;
+    
+
+	// Use this for initialization
+	void Start () {
+
+        HelpPage.SetActive(false);
+        OC = false;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+
+
+        if (Input.GetKeyDown(KeyCode.H))
+        {
+            Debug.Log("aaaaa");
+            Help();
+        }
+	
+	}
+
+    void Help()
+    {
+        if (OC == false)
+        {
+
+            HelpPage.SetActive(true);
+            OC = true;
+        }
+        else if (OC == true)
+        {
+            HelpPage.SetActive(false);
+            OC = false;
+        }
+    }
+
+    void Close()
+    {
+
+        if (OC == true)
+        {
+            HelpPage.SetActive(false);
+            OC = false;
+        }
+    }
+
+
+}
---
+++ Discovery/Assets/Scripts/UI/HelpSide.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HelpSide : MonoBehaviour {
+
+
+
+    public GameObject HelpPage;
+
+
+    // Use this for initialization
+    void Start () {
+
+        HelpPage.SetActive(false);
+
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void Open()
+    {
+
+        HelpPage.SetActive(true);
+
+    }
+
+    void Close()
+    {
+
+
+        HelpPage.SetActive(false);
+    }
+}
